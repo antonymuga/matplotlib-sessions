624d5664967d15116d30d6cd2efa9ae96946297c
antonymuga <anthony.ochieng.muga@gmail.com>
2023-05-29 14:39:23 +0300

    Created my first numpy script

--- plot1.py
+++ plot1.py
@@ -1,3 +1,12 @@
 import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
-print(matplotlib.__version__)
+xpoints = np.array([10, 68])
+ypoints = np.array([-1, 5])
+
+#plot() draws points which are known as markers in
+#matplotlib parlance
+plt.plot(xpoints, ypoints)
+
+plt.show()
