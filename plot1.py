001e3a5e9a10410b6f1910a1bd512034ef97148f
antonymuga <anthony.ochieng.muga@gmail.com>
2023-05-29 15:26:46 +0300

    Added markers script

--- plot1.py
+++ plot1.py
@@ -2,11 +2,11 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([10, 68])
-ypoints = np.array([-1, 5])
+xpoints = np.array([10, 34, 108, 68])
+ypoints = np.array([-1, 5, -9, 56])
 
 #plot() draws points which are known as markers in
 #matplotlib parlance
-plt.plot(xpoints, ypoints)
+plt.plot(xpoints)
 
 plt.show()
---
+++ plot2.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as ploti
+import numpy as nampi
+
+ypoints = nampi.array([12, 4, 36, 3])
+
+ploti.plot(ypoints, marker = "*")
+ploti.show()
