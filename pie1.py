88332dc706176a0033d2413518e42c92f49483ec
Antony Muga <anthony.ochieng.muga@gmail.com>
2023-06-02 06:44:21 +0300

    Added all source files

--- pie1.py
+++ pie1.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 y = np.array([30, 28, 24, 21])
+mylabels = ["Eva", "Antony", "Tom", "Mercy"]
+myexplode = [0.2, 0, 0, 0]
+mycolors = ["black", "grey", "#11abb0", "orange"]
 
-plt.pie(y)
+plt.pie(y, labels = mylabels, colors = mycolors, shadow = True, explode = myexplode, startangle = 45)
+plt.legend(title = "Muga K'Olale Children")
 plt.show()
