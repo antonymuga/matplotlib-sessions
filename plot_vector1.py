a5e3a0556630bd69babce48dbcb133bba7c6c171
antonymuga <anthony.ochieng.muga@gmail.com>
2024-01-26 15:12:07 +0300

    Fixed the keys

--- plot_vector1.py
+++ plot_vector1.py
@@ -7,15 +7,15 @@ vector_two = np.array([-1, 4])
 
 # Step 3: plot the vectors
 origin = np.zeros(2) #creates a 1-dimensional NumPy array with 2 elements
-myplt.quiver(*origin, *vector_one, scale=1, scale_units='xy', angles='xy', color='r', label='vector 1')
-myplt.quiver(*origin, *vector_two, scale=1, scale_units='xy', angles='xy', color='b', label='vector 2')
+myplt.quiver(*origin, *vector_one, scale=1, scale_units='xy', angles='xy', color='r', label='Male')
+myplt.quiver(*origin, *vector_two, scale=1, scale_units='xy', angles='xy', color='b', label='Female')
 # *origin: unpacks the arguements in origin by taking the elements in the array [0,0] and passes them to myplt.quiver), thus equal to 0,0
 # *vector_one: unpacks vector_one array and passes the args to myplt.quiver(), if vector_one is [2, 3], then it is equivalent to 2,3
 # scale=1: controls the length of the arrow, in this case length of arrow is proportional to magnitude
 # scale_units='xy': indicates the scale is applied to the x and y directions independently
 # angles='xy': specifies that arrow angles are defined by the x and y coordinated of the vectors
 # color='r', color='b': red and blue colors for the arrows respectively
-# label='vector 1': names of the labels 
+# label='vector 1': the values of the keys 
 
 # Set plot limits
 myplt.xlim(-5, 5)
